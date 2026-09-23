addedfa522fe6590f9a77f097531f70319989554
Mariia-6 <m2608851@edu.misis.com>
2026-09-23 13:15:27 +0300

    1

--- lab01/scr/06.py
+++ lab01/scr/06.py
@@ -1,14 +1,14 @@
-n = int(input())
+n = int(input('in_1:'))
 
 count_true = 0
 count_false = 0
 
 for i in range(n):
-    line = input(f'in_{i +1}:').split()
+    line = input(f'in_{i +2}:').split()
 
     if line[-1] == 'True':
         count_true += 1
     else:
         count_false += 1
 
-print(count_true,count_false)
+print('out:',count_true,count_false)
