8eb8e834f8925d8dc764baeb404cebac0686c9c6
Mariia-6 <m2608851@edu.misis.com>
2026-09-23 13:19:06 +0300

    1

--- lab01/scr/06.py
+++ lab01/scr/06.py
@@ -1,10 +1,10 @@
-n = int(input('in_1:'))
+n = int(input('in_1: '))
 
 count_true = 0
 count_false = 0
 
 for i in range(n):
-    line = input(f'in_{i +2}:').split()
+    line = input(f'in_{i +2}: ').split()
 
     if line[-1] == 'True':
         count_true += 1
